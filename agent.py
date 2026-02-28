d12b981b31b23caad29731de05ec1b5813caf585
Sushant-Panchal <thesushantpanchal07@gmail.com>
2026-03-01 02:32:14 +0530

    feat: Implemented RAG

--- agent.py
+++ agent.py
@@ -1,30 +1,61 @@
+# agent.py
+
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.agents import initialize_agent, AgentType
-from langchain.memory import ConversationBufferMemory
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from tools import WebSearchTool
 import os
+
+
+store = {}
+
+
+def get_session_history(session_id: str):
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
 
 class AgentFactory:
     def __init__(self):
         self.llm = ChatGoogleGenerativeAI(
             model="gemini-2.5-flash",
-            google_api_key=os.getenv("GEMINI_API_KEY")
+            google_api_key=os.getenv("GEMINI_API_KEY"),
+            temperature=0.3
         )
 
     def create_agent(self, system_prompt: str):
-        memory = ConversationBufferMemory(
-            memory_key="chat_history",
-            return_messages=True
+
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", system_prompt),
+            ("placeholder", "{history}"),
+            ("human", "{input}"),
+            ("placeholder", "{agent_scratchpad}")
+        ])
+
+        tools = [WebSearchTool]
+
+        agent = create_react_agent(
+            llm=self.llm,
+            tools=tools,
+            prompt=prompt
         )
 
-        agent = initialize_agent(
-            tools=[WebSearchTool],
-            llm=self.llm,
-            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-            memory=memory,
+        agent_executor = AgentExecutor(
+            agent=agent,
+            tools=tools,
             verbose=False,
             max_iterations=3,
             handle_parsing_errors=True
         )
 
-        return agent
+        agent_with_memory = RunnableWithMessageHistory(
+            agent_executor,
+            get_session_history,
+            input_messages_key="input",
+            history_messages_key="history"
+        )
+
+        return agent_with_memory
